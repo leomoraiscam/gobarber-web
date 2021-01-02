c8a35d5b82df4b5ab1c48698e2f1502addc6ca83
leomoraiscam <leozonnn@gmail.com>
2021-01-01 22:01:48 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <h1>Hello word</h1>
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
