ff1ebc6345e901b64ae0f28993ad1559826470ca
leomoraiscam <leozonnn@gmail.com>
2021-01-03 21:50:05 -0300

    validating the registration

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   FiMail, FiLock, FiUser, FiArrowLeft,
 } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+interface DataRegisterUser {
+  name: string;
+  email: string;
+  password: string,
+}
+
 const SignUp: React.FC = () => {
-  function handleSubmit(data: any): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: DataRegisterUser) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup
+          .string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup
+          .string()
+          .min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
