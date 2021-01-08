4637942fc4f92f10252be03b5a99bbe4bb3e54c4
leomoraiscam <leozonnn@gmail.com>
2021-01-07 22:14:39 -0300

    create header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { FiPower } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import {
+  Container, Header, HeaderContent, Profile,
+} from './styles';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  const x = 10;
+  const { signOut, user } = useAuth();
 
   return (
-    <h1 style={{ color: '#FFF' }}>Hello word</h1>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="logo" />
+          <Profile>
+            <img src="https://avatars0.githubusercontent.com/u/49538119?s=400&u=39a6291923942b4f7cc8fcb4bce259d116807701&v=4" alt="imagem de perfil" />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
   );
 };
 
