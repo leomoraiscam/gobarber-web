5e8f4ee0e387dc35f7a9f9132923aca1557e9b61
lmorais <leozonnn@gmail.com>
2021-02-07 16:17:52 -0300

    finishing hook test

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -96,10 +96,6 @@ const AuthProvider: React.FC = ({ children }) => {
 function useAuth(): AuthContextState {
   const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-
   return context;
 }
 
